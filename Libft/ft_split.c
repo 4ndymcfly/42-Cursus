2b0beb2623f4b42fcc5e00462fa7941bf053914c
4ndymcfly <4ndymcfly@gmail.com>
2024-09-27 12:42:51 +0200

    Project Libft

--- Libft/ft_split.c
+++ Libft/ft_split.c
@@ -6,17 +6,37 @@
 /*   By: andloren <andloren@student.42malaga.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/25 12:11:37 by andloren          #+#    #+#             */
-/*   Updated: 2024/09/26 10:01:24 by andloren         ###   ########.fr       */
+/*   Updated: 2024/09/27 12:34:01 by andloren         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "libft.h"
 
-
-
-
-
-
-
-char	**ft_split(char const *s, char c)
+int ft_count_words(char const *s, char c)
 {
+	int i;
+	int count;
+
+	i = 0;
+	count = 0;
+	while (s[i])
+	{
+		if (s[i] != c)
+		{
+			count++;
+			while (s[i] != c && s[i])
+				i++;
+		}
+		else
+			i++;
+	}
+	return (count);
 }
+
+/* int main()
+{
+	char *s = "  Hello  World  from      42   ";
+	char c = ' ';
+	printf("%d\n", ft_count_words(s, c));
+	return (0);
+} */
