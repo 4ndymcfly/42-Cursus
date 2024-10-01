13ace1cd3ad6e462f319d31d9ade2c1e67a1a2fc
4ndymcfly <4ndymcfly@gmail.com>
2024-10-01 14:17:19 +0200

    Project Libft

--- Libft/ft_lstsize_bonus.c
+++ Libft/ft_lstsize_bonus.c
@@ -6,7 +6,7 @@
 /*   By: andloren <andloren@student.42malaga.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/30 12:28:58 by andloren          #+#    #+#             */
-/*   Updated: 2024/09/30 12:29:07 by andloren         ###   ########.fr       */
+/*   Updated: 2024/10/01 14:16:39 by andloren         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,13 +14,35 @@
 
 int	ft_lstsize(t_list *lst)
 {
-	int	cnt;
+	int	count;
 
-	cnt = 0;
+	count = 0;
 	while (lst)
 	{
 		lst = lst->next;
-		cnt++;
+		count++;
 	}
-	return (cnt);
+	return (count);
 }
+
+/* int	main(void)
+{
+	t_list	*head;
+	t_list	*node1;
+	t_list	*node2;
+	t_list	*node3;
+	int		size;
+
+	node1 = ft_lstnew("Node 1");
+	node2 = ft_lstnew("Node 2");
+	node3 = ft_lstnew("Node 3");
+	head = node1;
+	ft_lstadd_front(&head, node2);
+	ft_lstadd_front(&head, node3);
+	size = ft_lstsize(head);
+	printf("Nodes in list: %d\n\n\n", size);
+	free(node1);
+	free(node2);
+	free(node3);
+	return (0);
+} */
