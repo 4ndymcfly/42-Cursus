c01f9dab728a0c6ea65af749a3f640fc19a16839
4ndymcfly <4ndymcfly@gmail.com>
2024-10-01 14:07:43 +0200

    Project Libft

--- Libft/ft_lstadd_front_bonus.c
+++ Libft/ft_lstadd_front_bonus.c
@@ -6,7 +6,7 @@
 /*   By: andloren <andloren@student.42malaga.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/30 12:28:38 by andloren          #+#    #+#             */
-/*   Updated: 2024/09/30 12:28:49 by andloren         ###   ########.fr       */
+/*   Updated: 2024/10/01 14:07:05 by andloren         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,3 +19,16 @@ void	ft_lstadd_front(t_list **lst, t_list *new)
 	new->next = *lst;
 	*lst = new;
 }
+
+/* int	main(void)
+{
+	t_list	*head;
+	t_list	*new_node;
+
+	head = NULL;
+	new_node = ft_lstnew("Hello, 42 students!");
+	ft_lstadd_front(&head, new_node);
+	printf("Node content: %s\n\n\n", (char *)head->content);
+	free(head);
+	return (0);
+} */
