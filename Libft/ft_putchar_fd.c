b78c601af122f7722b6449b14cf29686ffbf41bb
4ndymcfly <4ndymcfly@gmail.com>
2024-10-01 11:53:26 +0200

    Project Libft

--- Libft/ft_putchar_fd.c
+++ Libft/ft_putchar_fd.c
@@ -6,7 +6,7 @@
 /*   By: andloren <andloren@student.42malaga.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/30 12:04:45 by andloren          #+#    #+#             */
-/*   Updated: 2024/09/30 12:04:54 by andloren         ###   ########.fr       */
+/*   Updated: 2024/10/01 11:52:48 by andloren         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,3 +16,9 @@ void	ft_putchar_fd(char c, int fd)
 {
 	write(fd, &c, 1);
 }
+/* int	main(void)
+{
+	ft_putchar_fd('A', 1);  // 1 es el descriptor para stdout
+	ft_putchar_fd('\n', 1); // Salto de línea
+	return (0);
+} */
