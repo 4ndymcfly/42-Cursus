35cb8fa8f54893d51f85decb7323968907221ddd
4ndymcfly <4ndymcfly@gmail.com>
2024-10-01 13:12:01 +0200

    Project Libft

--- Libft/ft_lstnew_bonus.c
+++ Libft/ft_lstnew_bonus.c
@@ -6,7 +6,7 @@
 /*   By: andloren <andloren@student.42malaga.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/30 12:26:50 by andloren          #+#    #+#             */
-/*   Updated: 2024/09/30 12:37:58 by andloren         ###   ########.fr       */
+/*   Updated: 2024/10/01 13:11:03 by andloren         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,12 +14,25 @@
 
 t_list	*ft_lstnew(void *content)
 {
-	t_list	*lst;
+	t_list	*node;
 
-	lst = (t_list *)malloc(sizeof(t_list));
-	if (!lst)
+	node = (t_list *)malloc(sizeof(t_list));
+	if (!node)
 		return (NULL);
-	lst->content = content;
-	lst->next = 0;
-	return (lst);
+	node->content = content;
+	node->next = 0;
+	return (node);
 }
+/* int	main(void)
+{
+	t_list	*node;
+	char	*content;
+
+	content = "Hello, 42 students!";
+	node = ft_lstnew(content);
+	printf("Contenido del nodo: %s\n", (char *)node->content);
+	printf("Dirección del nodo: %p\n", (void *)node);
+	printf("Dirección del siguiente nodo: %p\n\n\n", (void *)node->next);
+	free(node);
+	return (0);
+} */
