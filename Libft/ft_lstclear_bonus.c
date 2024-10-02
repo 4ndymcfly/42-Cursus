23a27b69c1d7792e4c845619da53408ae16f7d63
4ndymcfly <4ndymcfly@gmail.com>
2024-10-02 11:32:46 +0200

    Project Libft

--- Libft/ft_lstclear_bonus.c
+++ Libft/ft_lstclear_bonus.c
@@ -6,7 +6,7 @@
 /*   By: andloren <andloren@student.42malaga.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/30 12:30:18 by andloren          #+#    #+#             */
-/*   Updated: 2024/09/30 12:30:20 by andloren         ###   ########.fr       */
+/*   Updated: 2024/10/02 11:31:47 by andloren         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,6 +17,8 @@ void	ft_lstclear(t_list **lst, void (*del)(void *))
 	t_list	*curr;
 	t_list	*next;
 
+	if (!lst || !del)
+		return ;
 	curr = *lst;
 	while (curr)
 	{
@@ -26,3 +28,35 @@ void	ft_lstclear(t_list **lst, void (*del)(void *))
 	}
 	*lst = NULL;
 }
+
+/* void	free_content(void *content)
+{
+	free(content);
+} */
+
+/* int	main(void)
+{
+	t_list	*head;
+	t_list	*node1;
+	t_list	*node2;
+
+	head = NULL;
+	node1 = ft_lstnew(malloc(10));
+	node2 = ft_lstnew(malloc(10));
+	if (!node1 || !node2)
+	{
+		free(node1);
+		free(node2);
+		return (1);
+	}
+	ft_strlcpy(node1->content, "Node 1", 10);
+	ft_strlcpy(node2->content, "Node 2", 10);
+	head = node1;
+	node1->next = node2;
+	ft_lstclear(&head, free_content);
+	if (head == NULL)
+		printf("List cleared successfully.\n\n\n");
+	else
+		printf("List not cleared.\n\n\n");
+	return (0);
+} */
